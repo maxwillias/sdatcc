3d10c89b299c32f44e1ae12aa30d934759c16c94
maxwillias <williasdfranquias@gmail.com>
2024-08-05 22:23:23 -0300

    :sparkles: feat: Cria o componente de Trabalho

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,92 +14,11 @@
             </div>
         </div>
 
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-4">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden p-4 shadow-sm sm:rounded-lg border-solid border-2 border-[#5ACC58]">
-                <b>Trabalho em concluído</b>
-                <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-2">
-                    <table class="w-full text-sm text-left rtl:text-right text-gray-700 dark:text-gray-400">
-                        <thead class="text-xs text-white uppercase bg-[#5ACC58] dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="px-6 py-3">
-                                    Título
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Aluno
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Orientador
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Data de publicação
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr class="bg-[#CCCFD4] h-[100px] border-b dark:bg-gray-800 dark:border-gray-700 text-gray-900">
-                                <th scope="row"
-                                    class="px-6 py-4 font-medium whitespace-nowrap dark:text-white">
-                                    Sistema de Gerenciamento de Trabalhos de Conclusão de Curso do IFNMG - Campus
-                                    Januária.
-                                </th>
-                                <td class="px-6 py-4">
-                                    Willias Júnior Farias de Souza
-                                </td>
-                                <td class="px-6 py-4">
-                                    Raphael Hoed Magalhães
-                                </td>
-                                <td class="px-6 py-4">
-                                    5 de Outubro de 2024
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
+        <x-trabalho titulo="Site para doação de Jacarézinhos" status="em Andamento" orientador="Raphael Magalhães"
+            data-publicacao="5 de Outubro 2023" aluno="Willias Júnior Farias de Souza" />
+        <x-trabalho titulo="Site para doação de doguinhos" status="Concluído" orientador="Joselixo"
+            data-publicacao="25 de Maio 2028" aluno="Willias Renan" />
 
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-4">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden p-4 shadow-sm sm:rounded-lg border-solid border-2 border-[#5ACC58]">
-                <b>Trabalho em concluído</b>
-                <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-2">
-                    <table class="w-full text-sm text-left rtl:text-right text-gray-700 dark:text-gray-400">
-                        <thead class="text-xs text-white uppercase bg-[#5ACC58] dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="px-6 py-3">
-                                    Título
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Aluno
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Orientador
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Data de publicação
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr class="bg-[#CCCFD4] h-[100px] border-b dark:bg-gray-800 dark:border-gray-700 text-gray-900">
-                                <th scope="row"
-                                    class="px-6 py-4 font-medium whitespace-nowrap dark:text-white">
-                                    Indisciplina 2D - Um jogo educacional voltado para tratar a indisciplina nas escolas.
-                                </th>
-                                <td class="px-6 py-4">
-                                    Willias Renan Silva
-                                </td>
-                                <td class="px-6 py-4">
-                                    Adriano Antunes Prates
-                                </td>
-                                <td class="px-6 py-4">
-                                    13 de Julho de 2025
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
     </div>
 
 </x-app-layout>
