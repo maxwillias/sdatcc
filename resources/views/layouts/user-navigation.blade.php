a7187d796aadab1d9c9613025649f5af2278e3ac
maxwillias <williasdfranquias@gmail.com>
2024-08-10 18:14:31 -0300

    :sparkles: feat: Adiciona o layout de usuario

---
+++ resources/views/layouts/user-navigation.blade.php
@@ -0,0 +1,37 @@
+<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+    <!-- Primary Navigation Menu -->
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between h-16">
+            <div class="flex">
+                <!-- Logo -->
+                <div class="shrink-0 flex items-center">
+                    <a href="{{ route('dashboard') }}">
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    </a>
+                </div>
+
+                <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Dashboard') }}
+                    </x-nav-link>
+                </div>
+
+                <div class="">
+
+                </div>
+            </div>
+            <div class="hidden sm:flex sm:items-center sm:ml-6">
+                <nav class="-mx-3 flex flex-1 justify-end">
+                    <a href="{{ route('login') }}" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700">
+                        Login
+                    </a>
+
+                    <a href="{{ route('register') }}" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700">
+                        Register
+                    </a>
+                </nav>
+            </div>
+        </div>
+    </div>
+</nav>
