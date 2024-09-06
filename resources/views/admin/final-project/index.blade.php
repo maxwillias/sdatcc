dc8ef143d8280413fa9171f387b61cc0f95c3a6b
maxwillias <williasdfranquias@gmail.com>
2024-09-06 17:02:05 -0300

    :sparkles: feat: Adiciona modal de excluir TCC no admin

--- resources/views/admin/final-project/index.blade.php
+++ resources/views/admin/final-project/index.blade.php
@@ -67,16 +67,45 @@
                     </li>
                 </ul>
                 <div class="py-2">
-                    <form action="{{ route('admin.final-projects.destroy', ['final_project' => $item]) }}" method="POST" id="form-destroy-{{ $item->id }}">
-                        @csrf
-                        @method('delete')
-                        <a href="javascript:void(0);" onclick="document.getElementById('form-destroy-{{ $item->id }}').submit()" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Excluir</a>
-                    </form>
+                    <a data-modal-target="popup-modal-{{ $item->id }}" data-modal-toggle="popup-modal-{{ $item->id }}" href="javascript:void(0);" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                        Excluir
+                    </a>
                 </div>
            </div>
+
+           {{-- Modal excluir --}}
+           <div id="popup-modal-{{ $item->id }}" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class="relative p-4 w-full max-w-md max-h-full">
+                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal-{{ $item->id }}">
+                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                            </svg>
+                            <span class="sr-only">Fechar modal</span>
+                        </button>
+                        <div class="p-4 md:p-5 text-center">
+                            <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                            </svg>
+                            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Tem certeza que deseja excluir esse Trabalho de conclusão de curso, esse processo é irreversível?</h3>
+                            <button data-modal-hide="popup-modal-{{ $item->id }}" type="button" class="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                                Não, cancelar
+                            </button>
+                            <form action="{{ route('admin.final-projects.destroy', ['final_project' => $item]) }}" method="POST" id="form-destroy-{{ $item->id }}" class="inline-flex">
+                                @csrf
+                                @method('delete')
+                                <button data-modal-hide="popup-modal-{{ $item->id }}" type="submit" class="text-white ms-3 bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                                    Sim, excluir
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         @endforeach
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-4">
             {{ $items->appends(request()->input())->links() }}
         </div>
     </div>
+
 </x-admin-layout>
